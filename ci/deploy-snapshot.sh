b7853bdac9b3e88f6af2b42b5b4b0f9d8af8d612
Thomas Broyer <t.broyer@ltgt.net>
2015-01-05 02:18:10 +0100

    Only deploy snapshots once in the matrix build
    …after testing with the latest release of GWT (2.7.0)

--- ci/deploy-snapshot.sh
+++ ci/deploy-snapshot.sh
@@ -1,5 +1,6 @@
 if [ "$TRAVIS_REPO_SLUG" == "tbroyer/gwt-maven-plugin" ] && \
    [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && \
+   [ "$GWT_VERSION" == "2.7.0" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
 
