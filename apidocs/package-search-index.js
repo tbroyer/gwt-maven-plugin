b8675fa96148a9f36dab92d8630d17f10460a9c9
Thomas Broyer <t.broyer@ltgt.net>
2023-08-22 14:15:45 +0200

    Wagon: Deploying site to repository

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"net.ltgt.gwt.maven"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"net.ltgt.gwt.maven"}];updateSearchResults();
